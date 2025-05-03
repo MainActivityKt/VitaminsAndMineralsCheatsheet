20fd6a9cc8a7cc9038c4911637e8e2f9f5ca746d
Safi <safire101100@outlook.com>
2025-05-03 22:28:24 +0430

    Added font, creating vitamins card

--- app/src/main/java/com/safire/vitaminsmineralscheatsheet/MainActivity.kt
+++ app/src/main/java/com/safire/vitaminsmineralscheatsheet/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.safire.vitaminsmineralscheatsheet.ui.theme.VitaminsMineralsCheatsheetTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +36,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             VitaminsMineralsCheatsheetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    VitaminMineralItem(
+                        scientificName = "Ascorbic Acid",
+                        name = "Vitamin C",
+                        description = "Something",
+                        image = R.drawable.ic_launcher_foreground,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +50,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun VitaminMineralItem(
+    scientificName: String,
+    name: String,
+    description: String,
+    @DrawableRes image: Int,
+    modifier: Modifier = Modifier
+
+) {
+    ElevatedCard (
+        shape = CardDefaults.elevatedShape,
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .height(160.dp)
+    ){
+        Column (
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth()
+        ){
+            Text(
+                text = scientificName,
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth() // make text center vertical
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 10.sp
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VitaminsMineralsCheatsheetTheme {
-        Greeting("Android")
+        Box (modifier = Modifier.fillMaxSize()) {
+            VitaminMineralItem(
+                scientificName = "Ascorbic Acid",
+                name = "Vitamin C",
+                description = "Boosts immunity",
+                image = R.drawable.ic_launcher_foreground
+            )
+        }
     }
 }
--- app/src/main/java/com/safire/vitaminsmineralscheatsheet/ui/theme/Type.kt
+++ app/src/main/java/com/safire/vitaminsmineralscheatsheet/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.safire.vitaminsmineralscheatsheet.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.safire.vitaminsmineralscheatsheet.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,12 +16,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+    ),
+    titleMedium =
+        TextStyle(
+        fontFamily = FontFamily(Font(R.font.tagesschrift_regular,FontWeight.Medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
@@ -30,5 +32,5 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+
 )
