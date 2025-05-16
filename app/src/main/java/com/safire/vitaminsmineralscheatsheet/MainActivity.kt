27a889cc0d6d2d257189a8277793c0999d49361f
Safi <safire101100@outlook.com>
2025-05-16 23:38:57 +0430

    - Added vitamin items to the DataSource, and images of foods high in each vitamins to drawable - Redesigned the vitaminsMineral card - Updated the strings.xml to reflect the latest vitamins information

--- app/src/main/java/com/safire/vitaminsmineralscheatsheet/MainActivity.kt
+++ app/src/main/java/com/safire/vitaminsmineralscheatsheet/MainActivity.kt
@@ -8,18 +8,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +41,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.ParagraphStyle
@@ -45,7 +56,9 @@ import com.safire.vitaminsmineralscheatsheet.model.DataSource
 import com.safire.vitaminsmineralscheatsheet.model.VitaminMineralItem
 import com.safire.vitaminsmineralscheatsheet.ui.theme.VitaminsMineralsCheatsheetTheme
 
+
 const val BULLET = "\u2022"
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,8 +76,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun VitaminsMineralsApp(modifier: Modifier = Modifier) {
-    LazyColumn (
-        modifier = modifier.padding(4.dp)){
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxHeight()
+    ) {
         items(items = DataSource.data) {
             VitaminMineralCard(it)
         }
@@ -75,7 +92,6 @@ fun VitaminsMineralsApp(modifier: Modifier = Modifier) {
 @Composable
 fun VitaminMineralCard(
     vitaminMineralItem: VitaminMineralItem,
-
     modifier: Modifier = Modifier
 
 ) {
@@ -83,8 +99,9 @@ fun VitaminMineralCard(
     val sources = stringResource(vitaminMineralItem.richSources).split("\n")
     val benefits = stringResource(vitaminMineralItem.benefits).split("\n")
 
-    ElevatedCard (
+    ElevatedCard(
         shape = CardDefaults.elevatedShape,
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
         modifier = modifier
             .fillMaxWidth()
             .clickable { expanded = !expanded }
@@ -92,10 +109,10 @@ fun VitaminMineralCard(
         Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
             modifier = Modifier
-                .padding(4.dp)
+                .padding(start = 8.dp, end = 8.dp, top = 4.dp, bottom = 4.dp)
                 .fillMaxWidth()
 
-        ){
+        ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
@@ -130,12 +147,12 @@ fun VitaminMineralCard(
 
             Spacer(modifier.size(4.dp))
 
-            if(expanded) {
+            if (expanded) {
                 Text(
                     "Rich food sources:",
                     fontWeight = FontWeight.SemiBold,
 
-                )
+                    )
 
 
                 val paragraphStyle = ParagraphStyle(textIndent = TextIndent(restLine = 12.sp))
@@ -153,8 +170,8 @@ fun VitaminMineralCard(
                 Spacer(modifier.size(4.dp))
                 Text(
                     buildAnnotatedString {
-                        Text("Health Benefits:", fontWeight = FontWeight.SemiBold,)
-                        sources.forEach {
+                        Text("Health Benefits:", fontWeight = FontWeight.SemiBold)
+                        benefits.forEach {
                             withStyle(style = paragraphStyle) {
                                 append(BULLET)
                                 append("\t\t")
@@ -164,8 +181,41 @@ fun VitaminMineralCard(
                     }
                 )
             }
+
+            ExpandCollapseButton(
+                onClick = { expanded = !expanded },
+                text = if (!expanded) "See more" else "see less",
+                icon = if (!expanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowUp,
+                contentDescription = if (!expanded) "Arrow down" else "Arrow up",
+                modifier = Modifier.fillMaxWidth() //TODO
+            )
+
         }
     }
+}
+
+@Composable
+fun ExpandCollapseButton(
+    onClick: () -> Unit,
+    icon: ImageVector,
+    contentDescription: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    FilledTonalIconButton(
+        onClick = onClick,
+        modifier = modifier
+
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+
+            ) {
+            Icon(imageVector = icon, contentDescription = contentDescription)
+            Text(text, fontWeight = FontWeight.SemiBold)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
