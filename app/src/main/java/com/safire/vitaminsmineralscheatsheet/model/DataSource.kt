264da4f53ba3a2998faf8119cfd77b6a8135fa49
Safi <safire101100@outlook.com>
2025-05-16 23:38:57 +0430

    - Added vitamin items to the DataSource, and images of foods high in each vitamins to drawable - Redesigned the vitaminsMineral card - Updated the strings.xml to reflect the latest vitamins information

--- app/src/main/java/com/safire/vitaminsmineralscheatsheet/model/DataSource.kt
+++ app/src/main/java/com/safire/vitaminsmineralscheatsheet/model/DataSource.kt
@@ -1,18 +1,49 @@
 package com.safire.vitaminsmineralscheatsheet.model
 
-import androidx.annotation.DrawableRes
 import com.safire.vitaminsmineralscheatsheet.R
 
 object DataSource {
     val data: List<VitaminMineralItem> = listOf(
-            VitaminMineralItem(
-                image = R.drawable.carrot,
-                name = R.string.vitamin_a_name,
-                imageDescription = R.string.carrot_description,
-                scientificNames = R.string.vitamin_a_scientificNames,
-                description = R.string.vitamin_a_desctiption,
-                richSources = R.string.vitamin_a_sources,
-                benefits = R.string.vitamin_a_benefits,
-            )
+        VitaminMineralItem(
+            image = R.drawable.carrot,
+            name = R.string.vitamin_a_name,
+            imageDescription = R.string.carrot_description,
+            scientificNames = R.string.vitamin_a_scientific_names,
+            description = R.string.vitamin_a_description,
+            richSources = R.string.vitamin_a_sources,
+            benefits = R.string.vitamin_a_benefits,
+        ), VitaminMineralItem(
+            image = R.drawable.seeds,
+            name = R.string.vitamin_b1_name,
+            imageDescription = R.string.seeds_description,
+            scientificNames = R.string.vitamin_b1_scientific_name,
+            description = R.string.vitamin_b1_description,
+            richSources = R.string.vitamin_b1_sources,
+            benefits = R.string.vitamin_b1_benefits
+        ), VitaminMineralItem(
+            image = R.drawable.dairy,
+            name = R.string.vitamin_b2_name,
+            imageDescription = R.string.dairy_description,
+            scientificNames = R.string.vitamin_b2_scientific_name,
+            description = R.string.vitamin_b2_description,
+            richSources = R.string.vitamin_b2_sources,
+            benefits = R.string.vitamin_b2_benefits
+        ), VitaminMineralItem(
+            image = R.drawable.red_mushrooms,
+            name = R.string.vitamin_b3_name,
+            imageDescription = R.string.seeds_description,
+            scientificNames = R.string.vitamin_b3_scientific_name,
+            description = R.string.vitamin_b3_description,
+            richSources = R.string.vitamin_b3_sources,
+            benefits = R.string.vitamin_b3_benefits
+        ), VitaminMineralItem(
+            image = R.drawable.banana,
+            name = R.string.vitamin_b6_name,
+            imageDescription = R.string.banana_description,
+            scientificNames = R.string.vitamin_b6_scientific_name,
+            description = R.string.vitamin_b6_description,
+            richSources = R.string.vitamin_b6_sources,
+            benefits = R.string.vitamin_b6_benefits
+        )
     )
 }
